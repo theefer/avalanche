02c5da4649ee274638090b72b201bfb603f1d20b
Sébastien Cevey <seb@cine7.net>
2014-04-17 15:40:52 +0100

    Fix uri template regexp

--- js/avalanche/util/uriTemplate.js
+++ js/avalanche/util/uriTemplate.js
@@ -1,7 +1,7 @@
 define(function() {
   // Note: for more extensive support, see http://tools.ietf.org/html/rfc6570
   return function uriTemplate(template, params) {
-    return template.replace(/{(.*?)}/g, function(match, varName) {
+    return template.replace(/\{(.*?)\}/g, function(match, varName) {
       var val = params[varName];
       if (!val) {
         throw new Error("Missing parameter for URI template variable: " + varName);
